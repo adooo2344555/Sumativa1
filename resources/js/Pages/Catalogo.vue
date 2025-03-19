e20b717f3a31f6e5918aafbe0c8b8d48ea7bb4cc
Brayan Adonay <brayanadonaypenatealas@gamil.com>
2025-03-19 16:51:20 -0600

    Se cambio que aparezca material en catalogo y cierre el modal de car ala hora de confirmar reserva

--- resources/js/Pages/Catalogo.vue
+++ resources/js/Pages/Catalogo.vue
@@ -135,6 +135,8 @@ const confirmarReservacion = async () => {
           user: user,
           detalleReservaciones: [],
         };
+          // Cerrar el modal automáticamente
+          cerrarModal();
       } catch (err) {
         console.error("Error al confirmar la reservacion", err);
         Swal.fire({
@@ -235,21 +237,25 @@ const cerrarModal = () => {
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div v-for="producto in filteredProducts" :key="producto.id" class="bg-white rounded-lg shadow-lg overflow-hidden p-4">
-          <Swiper :modules="[Navigation]" navigation class="h-40">
-            <SwiperSlide v-for="img in producto.imagenes" :key="img">
-              <img :src="`images/products/${img.nombre}`" class="w-full h-40 object-contain" />
-            </SwiperSlide>
-          </Swiper>
-          <div class="p-4">
-            <h2 class="text-xl font-semibold">{{ producto.nombre }}</h2>
-            <span class="text-sm text-blue-600">{{ producto.descripcion }}</span>
-            <p class="text-yellow-500 text-lg font-bold">${{ producto.precio }}</p>
-            <div class="flex flex-col md:flex-row md:space-x-4">
-              <input type="number" min="1" :value="cantidades[producto.id] || 1" @input="cantidades[producto.id] = Number($event.target.value)" class="border p-2 rounded w-full my-2 md:w-auto md:my-0" />
-              <button @click="agregarReservacion(producto, cantidades[producto.id] || 1)" class="bg-yellow-500 text-white px-4 py-2 rounded w-full md:w-auto">Agregar Reservacion</button>
-            </div>
-          </div>
-        </div>
+  <Swiper :modules="[Navigation]" navigation class="h-40">
+    <SwiperSlide v-for="img in producto.imagenes" :key="img">
+      <img :src="`images/products/${img.nombre}`" class="w-full h-40 object-contain" />
+    </SwiperSlide>
+  </Swiper>
+  <div class="p-4">
+    <h2 class="text-xl font-semibold">{{ producto.nombre }}</h2>
+    <span class="text-sm text-blue-600">
+      {{ producto.descripcion }}
+      <br />
+      <strong>Material:</strong> {{ producto.material }}
+    </span>
+    <p class="text-yellow-500 text-lg font-bold">${{ producto.precio }}</p>
+    <div class="flex flex-col md:flex-row md:space-x-4">
+      <input type="number" min="1" :value="cantidades[producto.id] || 1" @input="cantidades[producto.id] = Number($event.target.value)" class="border p-2 rounded w-full my-2 md:w-auto md:my-0" />
+      <button @click="agregarReservacion(producto, cantidades[producto.id] || 1)" class="bg-yellow-500 text-white px-4 py-2 rounded w-full md:w-auto">Agregar Reservacion</button>
+    </div>
+  </div>
+</div>
       </div>
 
       <div class="mt-6 flex justify-center">
